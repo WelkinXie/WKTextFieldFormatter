6a90078e2d53fbc8151e6d0105495d97de742193
WelkinXie <welkin995@126.com>
2016-04-23 20:04:56 +0800

    URL

--- WKTextFieldFormatter/AppDelegate.h
+++ WKTextFieldFormatter/AppDelegate.h
@@ -5,6 +5,8 @@
 //  Created by Welkin Xie on 15/12/25.
 //  Copyright © 2015年 WelkinXie. All rights reserved.
 //
+//  Github: https://github.com/WelkinXie/WKTextFieldFormatter
+//
 
 #import <UIKit/UIKit.h>
 
--- WKTextFieldFormatter/WKTextFieldFormatter/WKTextFieldFormatter.m
+++ WKTextFieldFormatter/WKTextFieldFormatter/WKTextFieldFormatter.m
@@ -4,6 +4,8 @@
 //  Created by Welkin Xie on 15/12/25.
 //  Copyright © 2015年 WelkinXie. All rights reserved.
 //
+//  Github: https://github.com/WelkinXie/WKTextFieldFormatter
+//
 
 #import "WKTextFieldFormatter.h"
 
